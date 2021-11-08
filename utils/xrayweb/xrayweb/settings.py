311fe10c3be78180685e908bc995f675ac9d3500
Arthur Vitui <avitui@redhat.com>
2021-11-08 17:18:40 -0500

    added django-mathfilter

--- utils/xrayweb/xrayweb/settings.py
+++ utils/xrayweb/xrayweb/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'xraylab.apps.XraylabConfig',
     'crispy_forms',
+    'mathfilters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
